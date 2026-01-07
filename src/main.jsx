a739bf6a3fdffb56d093f52a3b6e86c9cddd7e72
Quacky <66437768+quacky20@users.noreply.github.com>
2026-01-07 22:29:01 +0530

    Remove StrictMode wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <BrowserRouter basename="/~srijan2026">
       <App/>
     </BrowserRouter>
-
-  </StrictMode>,
 )
